f81af97d67e4f2a4e1135b8c221a649edd86f2af
ivan9paunov <vankatapaunov@gmail.com>
2025-03-04 00:38:41 +0200

    Add header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
+import Header from './components/header/Header.jsx';
+
 function App() {
     return (
         <>
-            <h1>FrameHunter</h1>
+            <Header />
         </>
     );
 }
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+import styles from './Header.module.css';
+
+export default function Header() {
+    return (
+        <header>
+            <input type="checkbox" id="mobileMenuToggler" className={styles.mobileTogglerCheckbox} />
+            <label htmlFor="mobileMenuToggler" className={styles.mobileToggler}>
+                <i className="fa-solid fa-bars"></i>
+            </label>
+            <div className={styles.logo}>
+                <a href="/home"><img src="/img/logo.png" alt="logo" className={styles['logo-img']} /></a>
+            </div>
+            <nav>
+                <div className={styles['nav-btns']}>
+                    <a href="/">Home</a>
+                    <a href="/catalog">Destinations</a>
+                    <a href="/add-frame">Add Frame</a>
+                    <a href="/profile">My Profile</a>
+                    <a href="/logout">Logout</a>
+                    {/* <a href="/login">Login</a> */}
+                    {/* <a href="/register">Register</a> */}
+                </div>
+            </nav>
+        </header>
+    );
+}
